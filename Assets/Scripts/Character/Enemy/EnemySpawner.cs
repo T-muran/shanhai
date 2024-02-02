70604d576e13a05ad4b365519dd1e7ed05b7f40b
zhou-bai <1219444196@qq.com>
2024-02-02 21:31:10 +0800

    敌人生成

---
+++ Assets/Scripts/Character/Enemy/EnemySpawner.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Configuration;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemyToSpawn;
+
+    public float timeToSpawn;
+    private float spawnCounter;
+
+    public Transform minSpawn, maxSpawn;
+
+    private Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnCounter = timeToSpawn;
+
+        target = PlayerHealthController.instance.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnCounter -= Time.deltaTime;
+        if(spawnCounter <= 0)
+        {
+            spawnCounter = timeToSpawn;
+
+            Instantiate(enemyToSpawn, SelectSpawnPoint(), transform.rotation);
+        }
+        transform.position = target.position;
+    }
+
+    public Vector3 SelectSpawnPoint()
+    {
+        Vector3 spawnPoint = Vector3.zero;
+        bool spawnVerticalEdge = Random.Range(0f, 1f) > .5f;
+
+        if (spawnVerticalEdge)
+        {
+            spawnPoint.y = Random.Range(minSpawn.position.y, maxSpawn.position.y);
+
+            if (Random.Range(0f, 1f) > .5f)
+            {
+                spawnPoint.x = maxSpawn.position.x;
+            }
+            else
+            {
+                spawnPoint.x = minSpawn.position.x;
+            }
+        }
+        else
+        {
+            spawnPoint.x = Random.Range(minSpawn.position.x, maxSpawn.position.x);
+
+            if (Random.Range(0f, 1f) > .5f)
+            {
+                spawnPoint.y = maxSpawn.position.y;
+            }
+            else
+            {
+                spawnPoint.y = minSpawn.position.y;
+            }
+        }
+
+
+
+        return spawnPoint;
+    }
+}
